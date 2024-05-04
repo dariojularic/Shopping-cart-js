4ebca9f53e1baea5d5d338c9b93d56f1d4b149e5
Dario Jularic <dariojularic0@gmail.com>
2024-05-04 20:31:58 +0200

    Create factory function

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ import { data } from './data.js'
 const carsContainer = document.querySelector(".cars")
 
 // izdvoji loop u funkciju updatecars i pozivaj je kad bude bilo promjene u arrayu cars
-// u kratorskoj funkciji koje ce funkcionalsnosti biti potrebne i napravi funkcije koje ce onavljati te operacije
+// u kratorskoj funkciji koje ce funkcionalsnosti biti potrebne i napravi funkcije koje ce obavljati te operacije
 // ispod kreatorske funkcije napravi setTImeout i setuj cars nakon jedne sekunde
 // pozovi update cars funkciju sa arrayem cars
+
 function shoppingCartFactory() {
-  let cars = [];
+  let carsArray = [];
+
+  const getCarsArray = () => carsArray;
+  const setCarsArray = (cars) => carsArray = cars;
+
+  const filterAvailable = (cars) => cars.filter((car) => car.available === "yes");
+  const filterNotAvailable = (cars) => cars.filter((car) => car.available === "no");
+
+  const sortPriceHighest = (cars) => cars.sort((b, a) => a.price - b.price);
+  const sortPriceLowest = (cars) => cars.sort((a, b) => a.price - b.price);
+  const sortAToZ = (cars) => cars.sort((a, b) => a.name.localeCompare(b.name));
+  const sortZToA = (cars) => cars.sort((b, a) => a.name.localeCompare(b.name));
+
+  return {getCarsArray, setCarsArray, filterAvailable, filterNotAvailable, sortPriceHighest, sortPriceLowest, sortAToZ, sortZToA}
+
 }
 
 // for (let i = 0; i < cars.length; i++) {

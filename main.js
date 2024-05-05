4bd1b01827bde07c71148a082ab3916781f8a55b
Dario Jularic <dariojularic0@gmail.com>
2024-05-05 09:02:25 +0200

    Create updateCars function

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // import './style.css'
 import { data } from './data.js'
 
-const carsContainer = document.querySelector(".cars")
-
-// izdvoji loop u funkciju updatecars i pozivaj je kad bude bilo promjene u arrayu cars
+// izdvoji loop u funkciju updateCars i pozivaj je kad bude bilo promjene u arrayu cars
 // u kratorskoj funkciji koje ce funkcionalsnosti biti potrebne i napravi funkcije koje ce obavljati te operacije
 // ispod kreatorske funkcije napravi setTImeout i setuj cars nakon jedne sekunde
 // pozovi update cars funkciju sa arrayem cars
+
+const carsContainer = document.querySelector(".cars")
+const carsUlList = document.querySelector(".cars-list")
 
 function shoppingCartFactory() {
   let carsArray = [];
@@ -23,9 +24,61 @@ function shoppingCartFactory() {
   const sortZToA = (cars) => cars.sort((b, a) => a.name.localeCompare(b.name));
 
   return {getCarsArray, setCarsArray, filterAvailable, filterNotAvailable, sortPriceHighest, sortPriceLowest, sortAToZ, sortZToA}
-
 }
 
+function updateCars(cars) {
+  for (let i = 0; i < cars.length; i++) {
+    const html = `<li class="car">
+                    <h2>${cars[i].name}</h2>
+                    <img src="${cars[i].image}"/>
+                    <div>
+                      <h4>Brand:</h4> <span>${cars[i].brand}</span>
+                      <h4>Manufactured Year:</h4> <span>${cars[i].manufacturedYear}</span>
+                      <h4>Doors:</h4> <span>${cars[i].doors}</span>
+                      <h4>Price:</h4> <span>${cars[i].price}</span>
+                    </div>
+                    <div>
+                      <h4>Available</h4> <span>${cars[i].available}</span>
+                    </div>
+                    <button class="delete-btn">
+                      Delete
+                    </button>
+                  </li>`;
+
+    carsUlList.insertAdjacentHTML	("afterbegin", html)
+    const listItem = document.querySelector(".car")
+    const btn = document.querySelector(".delete-btn");
+    btn.addEventListener("click", () => {
+      listItem.remove()
+    })
+  }
+}
+
+const carsList = shoppingCartFactory();
+carsList.setCarsArray(data)
+// setTimeout(carsList.setCarsArray(data), 1000)
+
+updateCars(carsList.getCarsArray())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const select = document.querySelector("#availability");
+// select.addEventListener("input", () => {
+//   console.log(select.value)
+// })
 // for (let i = 0; i < cars.length; i++) {
 
 //   const carItem = document.createElement("ul")
@@ -34,35 +87,28 @@ function shoppingCartFactory() {
 // }
 
 
+// const firstData = carsContainer.appendChild(document.createElement("div"))
+// firstData.classList = "first-data";
+// const h2 = firstData.appendChild(document.createElement("h2"));
+// const img = firstData.appendChild(document.createElement("img"));
+// const secondData = carsContainer.appendChild(document.createElement("div"));
+// secondData.classList = "second-data";
+// // const p = secondData.appendChild(document.createElement("p"));
+// const available = carsContainer.appendChild(document.createElement("p"));
+// const deleteButton = carsContainer.appendChild(document.createElement("button"));
 
-
-
-
-
-
-
-  // const firstData = carsContainer.appendChild(document.createElement("div"))
-  // firstData.classList = "first-data";
-  // const h2 = firstData.appendChild(document.createElement("h2"));
-  // const img = firstData.appendChild(document.createElement("img"));
-  // const secondData = carsContainer.appendChild(document.createElement("div"));
-  // secondData.classList = "second-data";
-  // // const p = secondData.appendChild(document.createElement("p"));
-  // const available = carsContainer.appendChild(document.createElement("p"));
-  // const deleteButton = carsContainer.appendChild(document.createElement("button"));
-
-  // // console.log(cars)
-  // h2.textContent = cars[i].name
-  // img.src = cars[i].image
-  // secondData.innerHTML = `<p>
-  // <strong>Brand:</strong> ${cars[i].brand}
-  // <strong>Manufactured Year:</strong> ${cars[i].manufacturedYear}
-  // <strong>Doors:</strong> ${cars[i].doors}
-  // <strong>Price:</strong> ${cars[i].price}
-  // </p>`;
-  // available.textContent = `Available: ${cars[i].available}`;
-  // deleteButton.textContent = "Delete"
-  // deleteButton.addEventListener("click", () => {
+// // console.log(cars)
+// h2.textContent = cars[i].name
+// img.src = cars[i].image
+// secondData.innerHTML = `<p>
+// <strong>Brand:</strong> ${cars[i].brand}
+// <strong>Manufactured Year:</strong> ${cars[i].manufacturedYear}
+// <strong>Doors:</strong> ${cars[i].doors}
+// <strong>Price:</strong> ${cars[i].price}
+// </p>`;
+// available.textContent = `Available: ${cars[i].available}`;
+// deleteButton.textContent = "Delete"
+// deleteButton.addEventListener("click", () => {
   //   console.log(cars.splice(i, 1))
   // }
 
@@ -73,3 +119,15 @@ function shoppingCartFactory() {
 
 
 // console.log(cars.length)
+
+// switch (select.value) {
+//   case "availability-yes":
+//     carsList.setCarsArray(carsList.filterAvailable(cars));
+//     break;
+//   case "availability-no":
+//     carsList.setCarsArray(carsList.filterNotAvailable(cars));
+//     break
+//   case "all"
+//     c
+// }
+// }
